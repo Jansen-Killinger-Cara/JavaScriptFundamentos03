cfb37632db3e85964425ef64fe61b775be44d8c5
Jansen <jansen@alunos.utfpr.edu.br>
2026-08-11 16:38:53 -0300

    Alterando arquivo funcao-simples da Aula 03

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,18 @@
+function saudacao() {
+    console.log("Olá, seja bem-vindo!");
+}
+
+saudacao(); // Chama a função saudacao e exibe a mensagem no console
+
+
+function bomdia() {
+    console.log("Bom dia!");
+}
+
+bomdia(); // Chama a função bomdia e exibe a mensagem no console
+
+function apresentar(nome) {
+    console.log("Olá, meu nome é " + nome + ".");
+}   
+
+apresentar("Jansen"); // Chama a função apresentar com o argumento "João" e exibe a mensagem no console
